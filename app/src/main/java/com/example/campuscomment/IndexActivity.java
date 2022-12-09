c0944807b107d4e9a8ef505dd085b214a7c88c75
majuzi <2318332846@qq.com>
2022-12-09 10:47:17 +0800

    初步完成第一个页面

--- app/src/main/java/com/example/campuscomment/IndexActivity.java
+++ app/src/main/java/com/example/campuscomment/IndexActivity.java
@@ -6,19 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class IndexActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
     }
 
-    public void firstclick(View view) {
+    public void searchview(View view) {
         //显式意图的使用方法实现页面的跳转
-        Intent intent=new Intent(MainActivity.this,IndexActivity.class);
+        Intent intent=new Intent(IndexActivity.this,SearchActivity.class);
         //执行意图
         startActivity(intent);
-
     }
 }
